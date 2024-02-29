6ab5965169114d69ec76cb9ce14e68bad62f0d57
socrater96 <157597749+socrater96@users.noreply.github.com>
2024-02-29 13:49:23 +0100

    29/02/24

---
+++ src/hacerTortilla/HacerTortilla.java
@@ -0,0 +1,35 @@
+/*El programa mostrará lo siguientes pasos:
+ * -Freír patatas
+ * -Calentar aceite
+ * -Pelar patatas
+ * -Poner sal
+ * -Trocear patatas
+ * -Poner la mezcla en la sartén
+ * -Mezclar patatas y huevo
+ * -Batir huevos
+ * -Romper cáscara de huevo
+ * 
+ * Luego se le preguntará al usuario que introduzcan los pasos en el orden correcto
+ * Si el usuario introduce un paso mal se le seguirá preguntando por lo siguientes
+ * Cuando se hayan introducido todos los pasos se le mostrarán todos los pasos 
+ * indicándosele cuales tiene mal y cuales son los que tendría que haber puesto.
+ */
+package hacerTortilla;
+import java.util.Scanner;
+public class HacerTortilla {
+	static String[] aleatorizar(String[] pasos) {
+		String[] aleatorio
+		return aleatorio;
+	}
+	
+	static void mostrarPasos(String pasos[]) {
+		
+	}
+	public static void main (String []args) {
+		Scanner in = new Scanner(System.in);
+		String pasos[]={"Calentar aceite","Pelar patatas","Trocear patatas",
+				"Freír patatas","Romper cáscara de los huevos","Batir huevos",
+				"Mezclar patas y huevo","Poner sal","Poner mezcla en la sartén",};
+		
+	}
+}
